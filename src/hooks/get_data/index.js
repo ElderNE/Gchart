e8b6eae7ca3446a43b47997c4c13896f1d1e9ea0
Eldar N <random24@yandex.ru>
2022-11-21 16:41:23 +0700

    change data host

--- src/hooks/get_data/index.js
+++ src/hooks/get_data/index.js
@@ -7,7 +7,7 @@ export const useGetData = () => {
     useEffect(() => {
         const req = async () => {
             try {
-                let request_data = await fetch('http://localhost:8010/proxy/tmp/test.php');
+                let request_data = await fetch('http://82.202.204.94/tmp/test.php',{mode:'cors'});
                 if(request_data.ok) {
                     let response_data = await request_data.json();
                     setDatalist(response_data);
